07953e4380e5ce0e5e4fa02ea054febd4213c167
Kathleen Wong <kwong17@stuy.edu>
2018-10-17 10:21:14 -0400

    part 2

--- linked_list.c
+++ linked_list.c
@@ -2,32 +2,62 @@
 #include <stdlib.h>
 #include <string.h>
 #include "linked_list.h"
-struct song_node{
-  char name[100];
-  char artist[100];
-  struct song_node *next;
-};
 
+song_node * root;
 void print_list(struct song_node * currentNode){
   while(currentNode){
-    printf("%c : % c ", currentNode->name, currentNode->artist);
+    printf("%s : %s ", currentNode->name, currentNode->artist);
     currentNode = currentNode->next;
   }
 }
 
 struct song_node * insert_front(struct song_node * root, struct song_node * add){
-  add = (struct song_node *add)malloc(sizeof(struct song_node));
+  add = (struct song_node *)malloc(sizeof(struct song_node));
   add -> next = root;
   return add;
 }
 void insert_order(struct song_node * add, struct song_node *root){
   insert_front(root,add);
   struct song_node * nextone = root;
-  while(strcmp(add->name,next->name) > 0){
+  while(strcmp(add->name,nextone->name) > 0){
     nextone = nextone -> next;
     add->next = nextone;
   }
 }
 
+void free_list(struct song_node *root ){
+  struct song_node * pointer;
+  while(root){
+    pointer = root->next;
+    free(root);
+    root = pointer;
+  }
+}
 
+struct song_node * return_song(char[] a, char[] s){
+  cur = root->next;
+  while(cur){
+    if (strcmp(cur->artist,a) == 0){
+      if(strcmp(cur->song,s) == 0){
+        return cur;
+      }
+    cur = cur->next;
+    }
+  }
+}
+
+struct song_node * first_song(char[] a){
+  cur = root->next;
+  while(cur){
+    if (strcmp(cur->artist,a) == 0){
+      return cur;
+    }
+    cur = cur->next;
+    }
+  }
+struct song_node * random_song(){
+  //FIX LATER!!!!!!!!!!!!!!!!!!!!!
+  srand(time(NULL));
+  num = rand();
+}
 //struct song_node * table[27];
--- linked_list.h
+++ linked_list.h
@@ -1,8 +1,13 @@
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
 void print_list(struct song_node * );
 struct song_node * insert_front(struct song_node *, struct song_node * );
 void insert_order(struct song_node *, struct song_node *);
-struct song_node * free_list(struct song_node * );
-struct song_node * return_song(char[] artist, char[] song);
-struct song_node * first_song(char[] artist);
+void free_list(struct song_node * );
+struct song_node * return_song(char[], char[]);
+struct song_node * first_song(char[]);
 struct song_node * random_song(struct song_node *);
 struct song_node * remove_song(struct song_node *);
