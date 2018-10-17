8e9a1e86d3cdde64c7c1647b03993f469fd1fda6
Kathleen Wong <kwong17@stuy.edu>
2018-10-17 16:33:48 -0400

    .h file?

---
+++ playlist.h
@@ -0,0 +1,9 @@
+void add(struct * song_node[27], struct song_node*);
+struct song_node * search_song(struct song_node[27], char * , char *);
+struct song_node * search_artist(struct song_node[27], char *);
+void print_letter (struct song_node[27], char * );
+void print_artist (struct song_node[27], char * );
+void print_library (struct * song_node[27]);
+void shuffle (struct * song_node[27]);
+void delete (struct * song_node[27], struct song_node*);
+void clear (struct * song_node[27]);
