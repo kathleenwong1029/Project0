69b5ed5a38628f38081c75431534f76486aa24de
Kathleen Wong <kwong17@stuy.edu>
2018-10-17 11:37:41 -0400

    hi tiff

--- linked_list.c
+++ linked_list.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include "linked_list.h"
 
-song_node * root;
+
 void print_list(struct song_node * currentNode){
   while(currentNode){
     printf("%s : %s ", currentNode->name, currentNode->artist);
@@ -34,30 +35,60 @@ void free_list(struct song_node *root ){
   }
 }
 
-struct song_node * return_song(char[] a, char[] s){
-  cur = root->next;
+struct song_node * return_song(char*a, char*s,struct song_node * root){
+  struct song_node *cur = root;
   while(cur){
     if (strcmp(cur->artist,a) == 0){
-      if(strcmp(cur->song,s) == 0){
+      if(strcmp(cur->name,s) == 0){
         return cur;
       }
-    cur = cur->next;
     }
+    cur = cur->next;
+
   }
+  return NULL;
 }
 
-struct song_node * first_song(char[] a){
-  cur = root->next;
+struct song_node * first_song(char*a, struct song_node *root){
+  struct song_node *cur = root->next;
   while(cur){
     if (strcmp(cur->artist,a) == 0){
       return cur;
     }
     cur = cur->next;
     }
+    return NULL;
   }
-struct song_node * random_song(){
-  //FIX LATER!!!!!!!!!!!!!!!!!!!!!
+int size(struct song_node *root){
+  struct song_node *cur = root;
+  int x = 0;
+  while (cur){
+    x++;
+    cur = cur->next;
+  }
+  return x;
+}
+struct song_node * random_song(struct song_node *root){
+  struct song_node *cur = root;
   srand(time(NULL));
-  num = rand();
+  int num = rand() % size(root);
+  while (num){
+    cur = cur->next;
+    num --;
+  }
+  return cur;
+}
+void remove_song(struct song_node *rem, struct song_node *root){
+  struct song_node *cur = root;
+  struct song_node *prev;
+  while(rem != cur && cur){
+    prev = cur;
+    cur = cur->next;
+
+  }
+  if (cur == root){
+    root = cur->next;
+  }
+  prev->next = cur->next;
 }
 //struct song_node * table[27];
