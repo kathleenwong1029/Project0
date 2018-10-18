8f813786297e8effb686d655ba2940d5d8f6797f
Kathleen Wong <kwong17@stuy.edu>
2018-10-18 11:29:17 -0400

    .

--- playlist.c
+++ playlist.c
@@ -2,7 +2,43 @@
 #include <stdlib.h>
 #include <string.h>
 #include "playlist.h"
-void add(struct song_node[27]insert, struct song_node x){
+void add(struct song_node* x){
+  char art[100]= x->artist;
+  char letter = art[0];
+  int num;
+  if (letter >= 'a' && letter <= 'z'){
+    num = letter - 'a';
+  }
+  else{
+    num = 26;
+  }
+  insert_order(x,table[num]) ;
+}
+
+struct song_node * search_song(char * art, char *song){
+  char letter = art[0];
+  int num;
+  if (letter >= 'a' && letter <= 'z'){
+    num = letter - 'a';
+  }
+  else{
+    num = 26;
+  }
+  return return_song(art, song, table[num]);
+}
+
+struct song_node * search_artist(, char * art){
+  char letter = art[0];
+  int num;
+  if (letter >= 'a' && letter <= 'z'){
+    num = letter - 'a';
+  }
+  else{
+    num = 26;
+  }
+  return first_song(art,table[num]);
+}
+void delete (struct song_node* x){
   char [] art = x->artist;
   char letter = art[0];
   int num;
@@ -12,61 +48,25 @@ void add(struct song_node[27]insert, struct song_node x){
   else{
     num = 26;
   }
-  insert_order(x,insert[num]) ;
+  remove_song(x,table[num]);
 }
-
-struct song_node * search_song(struct song_node[27] insert , char * art, char *song){
-  char letter = art[0];
-  int num;
+void print_letter (char *letter){
   if (letter >= 'a' && letter <= 'z'){
     num = letter - 'a';
   }
   else{
     num = 26;
   }
-  return return_song(art, song, insert[num]);
+  print_list(table[num]);
 }
-
-struct song_node * search_artist(struct song_node[27]insert, char *){
-  char letter = art[0];
-  int num;
+void print_artist (char * art){
   if (letter >= 'a' && letter <= 'z'){
     num = letter - 'a';
   }
   else{
     num = 26;
   }
-  return first_song(art,insert[num]);
-}
-void delete (struct * song_node[27] insert, struct song_node* x){
-  char [] art = x->artist;
-  char letter = art[0];
-  int num;
-  if (letter >= 'a' && letter <= 'z'){
-    num = letter - 'a';
-  }
-  else{
-    num = 26;
-  }
-  remove_song(x,insert[num]);
-}
-void print_letter (struct song_node[27]insert, char *letter){
-  if (letter >= 'a' && letter <= 'z'){
-    num = letter - 'a';
-  }
-  else{
-    num = 26;
-  }
-  print_list(insert[num]);
-}
-void print_artist (struct song_node[27] insert, char * art){
-  if (letter >= 'a' && letter <= 'z'){
-    num = letter - 'a';
-  }
-  else{
-    num = 26;
-  }
-  struct song_node * pointer = first_song(art, insert[num]);
+  struct song_node * pointer = first_song(art, table[num]);
   struct song_node * artist_songs[];
   while(strcmp(pointer->artist, art)){
     artist_songs.insert_order(pointer);
@@ -75,10 +75,24 @@ void print_artist (struct song_node[27] insert, char * art){
   print_list(artist_songs);
 
 }
-void print_library (struct * song_node[27] insert){
+void print_library (){
   for(char i = a; i<z; i++){
-  print_letter(insert,i);
+  print_letter(table,i);
 }
-  print_letter(insert,'?');
+  print_letter(table,'?');
 }
-void shuffle (struct * song_node[27]);
+void shuffle (struct song_node[27]){
+  srand(time(NULL));
+  int num = rand() % 27;
+  random_song(table[num]);
+  int num1 = rand() % 27;
+  random_song(table[num1]);
+  int num2 = rand() % 27;
+  random_song(table[num2]);
+}
+void clear (struct song_node[27]){
+  for(char i = a; i<z; i++){
+  free_list(table,i);
+}
+  free_list(table,'?');
+}
--- playlist.h
+++ playlist.h
@@ -1,9 +1,14 @@
-void add(struct * song_node[27], struct song_node*);
-struct song_node * search_song(struct song_node[27], char * , char *);
+struct song_node{
+  char name[100];
+  char artist[100];
+  struct song_node *next;
+};
+void add(struct song_node*);
+struct song_node * search_song(char * , char *);
 struct song_node * search_artist(struct song_node[27], char *);
-void print_letter (struct song_node[27], char * );
-void print_artist (struct song_node[27], char * );
-void print_library (struct * song_node[27]);
-void shuffle (struct * song_node[27]);
-void delete (struct * song_node[27], struct song_node*);
-void clear (struct * song_node[27]);
+void print_letter (char * );
+void print_artist ( char * );
+void print_library ();
+void shuffle ();
+void delete (struct song_node*);
+void clear ();
